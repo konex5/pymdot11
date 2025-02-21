d86982da7ba1ce8cfdd3efc5b8d0dc60708d383c
konex5 <fix@konex.dev>
2025-02-21 16:23:24 +0100

    black

--- tests/pyfhmdot/test_simulation_idmrg.py
+++ tests/pyfhmdot/test_simulation_idmrg.py
@@ -11,7 +11,13 @@ def test_idmrg_all():
         imps,
         "sh_xxz-hz_u1",
         {"Jxy": 1, "Jz": 1, "hz": 0},
-        {"chi_max": 10, "normalize": True, "eps_truncation": 1e-8, "dw_total": 0, "dw_one_serie": 0},
+        {
+            "chi_max": 10,
+            "normalize": True,
+            "eps_truncation": 1e-8,
+            "dw_total": 0,
+            "dw_one_serie": 0,
+        },
         size=12,
         conserve_total=6,
     )
