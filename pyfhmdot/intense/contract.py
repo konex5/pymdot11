ce46ee1b534f1b143f68656a51268febf0af349a
konex5 <fix@konex.dev>
2025-02-21 16:23:21 +0100

    add contract.py

--- pyfhmdot/intense/contract.py
+++ pyfhmdot/intense/contract.py
@@ -16,15 +16,35 @@ def contract_mps_mpo_mps_left(dst,mps_down,mpo,mps_up):
     #    | |
     # 0 -|_|-2
     #    1|
-    #tmp_left_bloc = {}
     multiply_mp(dst,tmp,mps_up,[0,1,3],[0,2,1])
-    # tmp_left_bloc
-    # tmp
-    #     _
+    # dst
     #    | |-2
     #    | |
     #    | |-1
     #    | |
     #    |_|-0
-    #    
-    
+
+ 
+def contract_mps_mpo_mps_right(dst,mps_down,mpo,mps_up):
+    mpo_left = {}
+    fuse_mp(mpo_left, mpo, 2)
+    tmp = {}
+    # mps_down
+    #    2|
+    # 0 -|_|-3
+    #    1|
+    multiply_mp(tmp,mps_down,mpo_left,[2],[1])
+    # tmp
+    #    4|
+    # 3 -| |
+    #    | |
+    # 0 -|_|-2
+    #    1|
+    #tmp_left_bloc = {}
+    multiply_mp(dst,tmp,mps_up,[1,2,4],[2,3,1])
+    # dst
+    # 2 -| |
+    #    | |
+    # 1 -| |
+    #    | |
+    # 0 -|_|
