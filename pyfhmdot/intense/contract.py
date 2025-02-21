8bebb8e27a91d6fcd2832e7c1c0b3da1aa9227d8
konex5 <fix@konex.dev>
2025-02-21 16:23:21 +0100

    add dmps contraction

--- pyfhmdot/intense/contract.py
+++ pyfhmdot/intense/contract.py
@@ -1,7 +1,7 @@
-from pyfhmdot.intense.mul_mp import multiply_mp, fuse_mp
+from pyfhmdot.intense.mul_mp import multiply_mp, fuse_mp, permute_blocs
 
 
-def contract_mps_mpo_mps_left_border(dst,mps_down,mpo,mps_up):
+def contract_dmps_mpo_dmps_left_border(dst,mps_down,mpo,mps_up):
     mpo_left = {}
     fuse_mp(mpo_left, mpo,0)
     tmp = {}
@@ -19,13 +19,11 @@ def contract_mps_mpo_mps_left_border(dst,mps_down,mpo,mps_up):
     multiply_mp(dst,tmp,mps_up,[0,1,3],[0,2,1])
     # dst
     #    | |-2
-    #    | |
     #    | |-1
-    #    | |
     #    |_|-0
 
  
-def contract_mps_mpo_mps_right_border(dst,mps_down,mpo,mps_up):
+def contract_dmps_mpo_dmps_right_border(dst,mps_down,mpo,mps_up):
     mpo_left = {}
     fuse_mp(mpo_left, mpo, 2)
     tmp = {}
@@ -44,13 +42,11 @@ def contract_mps_mpo_mps_right_border(dst,mps_down,mpo,mps_up):
     multiply_mp(dst,tmp,mps_up,[1,2,4],[2,3,1])
     # dst
     # 2 -| |
-    #    | |
     # 1 -| |
-    #    | |
     # 0 -|_|
 
  
-def contract_left_bloc(dst,left_bloc, mps_down,mpo,mps_up):
+def contract_left_bloc_dmps(dst,left_bloc, mps_down,mpo,mps_up):
     tmp = {}
     # mps_down
     #    2|
@@ -72,13 +68,11 @@ def contract_left_bloc(dst,left_bloc, mps_down,mpo,mps_up):
     multiply_mp(dst,tmp_tmp,mps_up,[0,1,3],[0,2,1])
     # dst
     #    | |- 2
-    #    | |
     #    | |- 1
-    #    | |
     #    |_|- 0
 
 
-def contract_right_bloc(dst,right_bloc, mps_down,mpo,mps_up):
+def contract_right_bloc_dmps(dst,right_bloc, mps_down,mpo,mps_up):
     tmp = {}
     # mps_down
     #    2|
@@ -100,8 +94,33 @@ def contract_right_bloc(dst,right_bloc, mps_down,mpo,mps_up):
     multiply_mp(dst,tmp_tmp,mps_up,[0,2,4],[3,2,1])
     # dst
     # 2 -| |
-    #    | |
     # 1 -| |
-    #    | |
     # 0 -|_|
 
+
+def contract_left_right_bloc(dst,left_blocs,right_blocs):
+    multiply_mp(dst,left_blocs,right_blocs,[0,1,2],[0,1,2])
+    
+
+def contract_left_right_bloc_with_mpo(dst,left_blocs,mpo,right_blocs):
+    tmp = {}
+    # mpo
+    #    2|
+    # 0 -|_|-3
+    #    1|
+    multiply_mp(tmp,left_blocs,mpo,[1],[0])
+    # tmp
+    #  1-| ||3    
+    #    |   |-4
+    #  0-|_||2
+    tmp_tmp = {}
+    multiply_mp(tmp_tmp,tmp,right_blocs,[4],[1])
+    # tmp_tmp
+    #  1-| |_ |3_| |-5  
+    #    |  _   _  |
+    #  0-|_|  |2 | |-4
+    permute_blocs(dst,tmp_tmp,[(0,1,2,3,4,5),(0,2,4,1,3,5)])
+    # dst
+    #  3-| |_ |4_| |-5  
+    #    |  _   _  |
+    #  0-|_|  |1 | |-2
--- tests/pyfhmdot/intense/test_contract_crazy.py
+++ tests/pyfhmdot/intense/test_contract_crazy.py
@@ -2,42 +2,47 @@ import pytest
 
 def test_contract_left():
     from pyfhmdot.models.pymodels import hamiltonian_obc
-    from pyfhmdot.intense.contract import contract_mps_mpo_mps_left_border, contract_left_bloc
+    from pyfhmdot.intense.contract import contract_dmps_mpo_dmps_left_border, contract_left_bloc_dmps
     mpo = hamiltonian_obc("sh_xxz-hz_u1", {"Jxy": 0, "Jz": 0, "hz": 10}, 3)
     mps_down_left = mpo[0]
     mps_up_left = mpo[0]
     mpo_left = mpo[0]
     dst = {}
-    contract_mps_mpo_mps_left_border(dst,mps_down_left,mpo_left,mps_up_left)
+    contract_dmps_mpo_dmps_left_border(dst,mps_down_left,mpo_left,mps_up_left)
     assert [len(_) for _ in dst.keys()][0] == 3
     #
     mps_down = mpo[1]
     mps_up = mpo[1]
     mpo_middle = mpo[1]
     new_dst = {}
-    contract_left_bloc(new_dst,dst,mps_down,mpo_middle,mps_up)
+    contract_left_bloc_dmps(new_dst,dst,mps_down,mpo_middle,mps_up)
     assert [len(_) for _ in new_dst.keys()][0] == 3
     return new_dst
 
 def test_contract_right():
     from pyfhmdot.models.pymodels import hamiltonian_obc
-    from pyfhmdot.intense.contract import contract_mps_mpo_mps_right_border, contract_right_bloc
+    from pyfhmdot.intense.contract import contract_dmps_mpo_dmps_right_border, contract_right_bloc_dmps
     mpo = hamiltonian_obc("sh_xxz-hz_u1", {"Jxy": 0, "Jz": 0, "hz": 10}, 3)
     mps_down_left = mpo[-1]
     mps_up_left = mpo[-1]
     mpo_left = mpo[-1]
     dst = {}
-    contract_mps_mpo_mps_right_border(dst,mps_down_left,mpo_left,mps_up_left)
+    contract_dmps_mpo_dmps_right_border(dst,mps_down_left,mpo_left,mps_up_left)
     assert [len(_) for _ in dst.keys()][0] == 3
     #
     mps_down = mpo[1]
     mps_up = mpo[1]
     mpo_middle = mpo[1]
     new_dst = {}
-    contract_right_bloc(new_dst,dst,mps_down,mpo_middle,mps_up)
+    contract_right_bloc_dmps(new_dst,dst,mps_down,mpo_middle,mps_up)
     assert [len(_) for _ in new_dst.keys()][0] == 3
     return new_dst
 
 def test_contract_middle():
+    from pyfhmdot.intense.contract import contract_left_right_bloc
+
     left = test_contract_left()
     right = test_contract_right()
+    dst={}
+    contract_left_right_bloc(dst,left,right)
+    assert dst[()][()] == 0.0
