9ac0b4c3a2163c5876197150cba18093064e53b4
konex5 <fix@konex.dev>
2025-02-21 16:23:20 +0100

    it sweeps 11 times! Yahoo

--- tests/pyfhmdot/test_sweep_eleven.py
+++ tests/pyfhmdot/test_sweep_eleven.py
@@ -1,8 +1,11 @@
 import pytest
 
 
-@pytest.mark.skip("this test is a visual test with print")
-def test_eleven_times():
+# @pytest.mark.skip("this test is a visual test with print")
+def test_eleven_times(
+    make_maximal_entangled_state_u1,
+    make_single_dummy_dgate,
+):
     from pyfhmdot.algorithm import sweep_eleven_times
 
     # L=6; start_odd_bonds=True; start_left=True # ok for even and odd sites
@@ -15,3 +18,19 @@ def test_eleven_times():
     start_odd_bonds = False
     start_left = False  # ok for even and odd sites
     # sweep_eleven_times(L,L*[None], [(L-1)*[None],(L-1)*[None],(L-1)*[None],(L-1)*[None]],{"dw_one_serie":0, "dw_total": 0},10,True,10**-8,start_left=start_left,start_odd_bonds=start_odd_bonds)
+
+    ## real test
+    dmps = make_maximal_entangled_state_u1(L=11)
+    mpo = make_single_dummy_dgate()
+    ggate = [11 * [mpo], 11 * [mpo], 11 * [mpo], 11 * [mpo]]
+    sweep_eleven_times(
+        11,
+        dmps,
+        ggate,
+        {"dw_one_serie": 0, "dw_total": 0},
+        10,
+        True,
+        10 ** -8,
+        start_left=True,
+        start_odd_bonds=start_odd_bonds,
+    )
