c937be9f3ccbc3413ee44ef5985de91a95bbb9d7
konex5 <fix@konex.dev>
2025-02-21 16:23:18 +0100

    skip some dependencies

--- tests/pyfhmdot/test_dependencies.py
+++ tests/pyfhmdot/test_dependencies.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 @pytest.mark.skip(msg="module fhmdot not ready yet")
 def test_example():
     from example import view_nocopy
@@ -17,3 +18,10 @@ def test_scipy():
     import scipy
 
     assert "import numpy worked!" == "import numpy worked!"
+
+
+@pytest.mark.skip(msg="matplotlib is needed for visualizing benchmarks")
+def test_scipy():
+    import matplotlib.pyplot
+
+    assert "import matplotlib worked!" == "import matplotlib worked!"
