8af7dea6e4362943b8a280110c580f2ecc9813b2
konex5 <fix@konex.dev>
2025-02-21 16:23:22 +0100

    fix rel path

--- script/all.sh
+++ script/all.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-./cli/cli_create_hamiltonian.py -i ./tests/example/model.toml -o /tmp/hamiltonian.h5
-./cli/cli_create_hamiltonian_gates.py -i tests/example/model.toml -o /tmp/hamiltonian_gates.h5
-./cli/cli_create_maximal_entangled_state.py -H /tmp/hamiltonian.h5 -o /tmp/2B_00.0000.h5
-
-./cli/cli_Tdmrg.py -G /tmp/hamiltonian_gates.h5 -M /tmp/2B_00.0000.h5 -o /tmp/
-./cli/cli_tdmrg.py -G /tmp/hamiltonian_gates.h5 -M /tmp/2B_00.0250.h5 -o /tmp/
-
-./cli/cli_mbracket.py -b /tmp/2B_00.0250.h5 -k /tmp/2B_00.0250_00.0125.h5
+./script/cli_create_hamiltonian.py -i ./tests/example/model.toml -o /tmp/hamiltonian.h5
+./script/cli_create_hamiltonian_gates.py -i tests/example/model.toml -o /tmp/hamiltonian_gates.h5
+./script/cli_create_maximal_entangled_state.py -H /tmp/hamiltonian.h5 -o /tmp/2B_00.0000.h5
+./script/cli_Tdmrg.py -G /tmp/hamiltonian_gates.h5 -M /tmp/2B_00.0000.h5 -o /tmp/
+./script/cli_tdmrg.py -G /tmp/hamiltonian_gates.h5 -M /tmp/2B_00.0250.h5 -o /tmp/
+./script/cli_mbracket.py -b /tmp/2B_00.0250.h5 -k /tmp/2B_00.0250_00.0125.h5
 
