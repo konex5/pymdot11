9556efe6ae68c4f3a93e4158278b7fbc5a640858
konex5 <fix@konex.dev>
2025-02-21 16:23:18 +0100

    add more tests for sweep functionalities

--- tests/pyfhmdot/test_sweep.py
+++ tests/pyfhmdot/test_sweep.py
@@ -6,3 +6,15 @@ def test_sweeps():
 
     for i, j in enumerate(sweep(5)):
         assert i + 1 == j
+
+    for i, j in enumerate(sweep(5, from_site=3)):
+        assert i + 2 + 1 == j
+
+    for i, j in enumerate(sweep(5, to_site=4)):
+        assert i + 1 == j
+    assert j + 1 == 4
+
+    for i, j in enumerate(sweep(5, from_site=3, to_site=4)):
+        assert i + 2 + 1 == j
+
+    assert j + 1 == 4
