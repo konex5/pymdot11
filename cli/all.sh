84d29b2dba1c4305916573f35c2101b268fce1ce
konex5 <fix@konex.dev>
2025-02-21 16:23:20 +0100

    add all.sh script to run all scripts

---
+++ cli/all.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+
+./cli/cli_create_hamiltonian.py -i ./tests/example/model.toml -o /tmp/hamiltonian.h5
+./cli/cli_create_maximal_entangled_state.py -H /tmp/hamiltonian.h5 -o /tmp/2B_00.0000.h5
