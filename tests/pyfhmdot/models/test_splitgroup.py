442345aaf444a1339c17f86c9dd5d8809796d207
konex5 <fix@konex.dev>
2025-02-21 16:23:21 +0100

    add splitgroup

---
+++ tests/pyfhmdot/models/test_splitgroup.py
@@ -0,0 +1,22 @@
+import pytest
+
+
+def test_mapping():
+    from pyfhmdot.models.splitgroup import splitgroup_mapping
+
+    assert splitgroup_mapping("sh_xxz-hz_u1", section="deg")[0] == 2
+
+
+def test_reshape_group(make_maximal_entangled_state_u1):
+    import numpy as np
+    from pyfhmdot.models.splitgroup import reshape_and_split, reshape_and_group
+
+    dmps = make_maximal_entangled_state_u1(3)[0]
+    dst_blocs = {}
+
+    reshape_and_split("sh_blabla_u1", dst_blocs, dmps, 1)
+    assert dst_blocs[(0, 0, 0, 0)][0, 0, 0, 0] == 1 / np.sqrt(2)
+
+    dst_dst_blocs = {}
+    reshape_and_group("sh_blabla_u1", dst_dst_blocs, dst_blocs, [1, 2])
+    assert np.all(dst_dst_blocs[(0, 1, 0)][0, :, 0] == dmps[(0, 1, 0)][0, :, 0])
