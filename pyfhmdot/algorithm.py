3334898d0aee540ac125951921a17bc5136378c8
konex5 <fix@konex.dev>
2025-02-21 16:23:21 +0100

    add algorithm

--- pyfhmdot/algorithm.py
+++ pyfhmdot/algorithm.py
@@ -132,22 +132,26 @@ def should_go_left(size, layer, position, start_left):
         return False
 
 
-def should_apply_gate(size, layer, position, start_left, start_odd_bonds):
-    is_even = size % 2 == 0
-    is_currently_start_left = (layer % 2 == 0) == start_left
-    is_currently_applying_left = (position % 2 == 1) == start_odd_bonds
-
-    if is_currently_start_left:
-        return is_currently_applying_left
+def should_apply_gate(position, start_odd_bonds):
+    if start_odd_bonds:
+        start_odd_bonds = 0
     else:
-        if is_even:
-            return not is_currently_applying_left
-        else:
-            return is_currently_applying_left
+        start_odd_bonds = 1
 
+    return ( position + start_odd_bonds) % 2 == 0 
+    
+def sweep_on_layer(size,layer,start_left):
+    if start_left:
+        start_left = 0
+    else:
+        start_left = 1
+
+    if (layer + start_left % 2) == 0:
+        return sweep(size,from_site=0, to_site=size-2)
+    else:
+        return sweep(size,from_site=size-2, to_site=0)
 
 def sweep_eleven_times_easy(
-    size,
     mps,
     ggate,
     dw_dict,
@@ -157,7 +161,47 @@ def sweep_eleven_times_easy(
     start_left=True,
     start_odd_bonds=True,
 ):
-    pass
+    size = len(mps)
+
+    for layer in range(11):
+        print(f"suzuki_trotter fourth order, layer {layer+1}/11")
+        if layer in [0, 10]:
+            gate = ggate[0]
+        elif layer in [4, 6]:
+            gate = ggate[2]
+        elif layer in [5]:
+            gate = ggate[3]
+        else:  # [1, 2, 3, 7, 8, 9]:
+            gate = ggate[1]
+        
+        for l in sweep_on_layer(size,layer,start_left):
+            if should_apply_gate(l,start_odd_bonds):
+                apply_gate_on_mm_at(
+                    mps,
+                    gate,
+                    l+1,
+                    dw_dict,
+                    chi_max,
+                    normalize,
+                    eps,
+                    is_um=start_left,
+                    conserve_left_right_before=False,
+                    conserve_direction_left_after=start_left,
+                )
+            else:
+                apply_mm_at(
+                    mps,
+                    l+1,
+                    dw_dict,
+                    chi_max,
+                    normalize,
+                    eps,
+                    is_um=start_left,
+                    conserve_left_right_before=False,
+                    conserve_direction_left_after=start_left,
+                )
+        start_left = not start_left
+        start_odd_bonds = not start_odd_bonds
 
 
 def sweep_eleven_times(
