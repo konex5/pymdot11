a1c10e87ef6196b0c04d17fbb33048ebf045d3be
konex5 <fix@konex.dev>
2025-02-21 16:23:19 +0100

    fix few words and org

--- pyfhmdot/contract.py
+++ pyfhmdot/contract.py
@@ -149,16 +149,16 @@ def multiply_blocs_with_gate_applied(
     conserve_left_right_before: bool = False,
     conserve_left_right_after: bool = False
 ) -> None:
+    tmp_blocs: _Dict[tuple, _np.ndarray] = {}
     tmp_indices = indices_prepare_destination_without_gate(
         lhs_blocs.keys(),
         rhs_blocs.keys(),
         conserve_left_right=conserve_left_right_before,
     )
-    tmp_blocs: _Dict[tuple, _np.ndarray] = {}
     multiply_arrays(tmp_blocs, lhs_blocs, rhs_blocs, tmp_indices)
-    dest_indices = indices_theta_prepare_conservation_for_gate(
+    dst_indices = indices_theta_prepare_conservation_for_gate(
         tmp_blocs.keys(),
         gate_blocs.keys(),
         conserve_left_right=conserve_left_right_after,
     )
-    multiply_arrays_and_transpose(dst_blocs, tmp_blocs, gate_blocs, dest_indices)
+    multiply_arrays_and_transpose(dst_blocs, tmp_blocs, gate_blocs, dst_indices)
