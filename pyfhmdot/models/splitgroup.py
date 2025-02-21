6dcd42b8e5529397e602c4788c266a927b63e2ac
konex5 <fix@konex.dev>
2025-02-21 16:23:21 +0100

    add splitgroup

---
+++ pyfhmdot/models/splitgroup.py
@@ -0,0 +1,164 @@
+import numpy as _np
+
+
+def splitgroup_mapping(model_name, *, section):
+    head, _, tail = model_name.split("_")
+    mapping_name = head + "_" + tail
+    mapping = {
+        "sh_no": {"deg": [2], "2deg": [4], "map": [[(0, 0)], [0], [slice(0, 4)]]},
+        "sh_u1": {
+            "deg": [1, 1],
+            "2deg": [1, 2, 1],
+            "map": [
+                [(0, 0), (0, 1), (1, 0), (1, 1)],
+                [1, 0, 2, 1],
+                [slice(0, 1), slice(0, 1), slice(0, 1), slice(1, 2)],
+            ],
+        },
+        "so_no": {"deg": [3], "2deg": [9], "map": [[(0, 0)], [0], [slice(0, 9)]]},
+        "so_u1": {
+            "deg": [1, 1, 1],
+            "2deg": [1, 2, 3, 2, 1],
+            "map": [
+                [
+                    (0, 0),
+                    (0, 1),
+                    (0, 2),
+                    (1, 0),
+                    (1, 1),
+                    (1, 2),
+                    (2, 0),
+                    (2, 1),
+                    (2, 2),
+                ],
+                [2, 1, 0, 3, 2, 1, 4, 3, 2],
+                [
+                    slice(0, 1),
+                    slice(0, 1),
+                    slice(0, 1),
+                    slice(0, 1),
+                    slice(1, 2),
+                    slice(1, 2),
+                    slice(0, 1),
+                    slice(1, 2),
+                    slice(2, 3),
+                ],
+            ],
+        },
+        "ru_no": {"deg": [4], "2deg": [16], "map": [[(0, 0)], [0], [slice(0, 16)]]},
+        "ru_u1": {
+            "deg": [1, 2, 1],
+            "2deg": [1, 4, 6, 4, 1],
+            "map": [
+                [
+                    (0, 0),
+                    (0, 1),
+                    (0, 2),
+                    (1, 0),
+                    (1, 1),
+                    (1, 2),
+                    (2, 0),
+                    (2, 1),
+                    (2, 2),
+                ],
+                [2, 1, 0, 3, 2, 1, 4, 3, 2],
+                [
+                    slice(0, 1),
+                    slice(0, 2),
+                    slice(0, 1),
+                    slice(0, 2),
+                    slice(1, 5),
+                    slice(2, 4),
+                    slice(0, 1),
+                    slice(2, 4),
+                    slice(5, 6),
+                ],
+            ],
+        },
+    }
+    return mapping[mapping_name][section]
+
+
+def reshape_and_group(model_name, newblocks, oldblocks, indices):
+    mapping = splitgroup_mapping(model_name, section="map")
+    target_constructed = []
+    for it in oldblocks.keys():
+        tup2group = (it[indices[0]], it[indices[1]])
+        idmap = mapping[0].index(tup2group)
+        valuegrouped = mapping[1][idmap]
+        dest_key = tuple(
+            [it[i] for i in range(indices[0])]
+            + [valuegrouped]
+            + [it[i] for i in range(indices[1] + 1, len(it))]
+        )
+        oldshape = oldblocks[it].shape
+        if dest_key not in target_constructed:
+            totalshape = tuple(
+                [oldshape[i] for i in range(indices[0])]
+                + [splitgroup_mapping(model_name, section="2deg")[valuegrouped]]
+                + [oldshape[i] for i in range(indices[1] + 1, len(it))]
+            )
+            newblocks[dest_key] = _np.ndarray((totalshape), oldblocks[it].dtype)
+            newblocks[dest_key].fill(0)
+            target_constructed.append(dest_key)
+
+        newshape = tuple(
+            [oldshape[i] for i in range(indices[0])]
+            + [oldshape[indices[0]] * oldshape[indices[1]]]
+            + [oldshape[i] for i in range(indices[1] + 1, len(oldshape))]
+        )
+        newsliced = tuple(
+            [slice(0, oldshape[i]) for i in range(indices[0])]
+            + [mapping[2][idmap]]
+            + [slice(0, oldshape[i]) for i in range(indices[1] + 1, len(oldshape))]
+        )
+
+        newblocks[dest_key][newsliced] = oldblocks[it].reshape(newshape)
+
+
+def reshape_and_split(model_name, newblocks, oldblocks, index):
+    mapping = splitgroup_mapping(model_name, section="map")
+    for it in oldblocks.keys():
+        int2split = it[index]
+        if mapping[1].count(int2split) == 1:
+            idmap = mapping[1].index(int2split)
+            valuesplited = mapping[0][idmap]
+            dest_key = tuple(
+                [it[i] for i in range(index)]
+                + list(valuesplited)
+                + [it[i] for i in range(index + 1, len(it))]
+            )
+            oldshape = oldblocks[it].shape
+            newshape = tuple(
+                [oldshape[i] for i in range(index)]
+                + [int(_np.sqrt(oldshape[index])), int(_np.sqrt(oldshape[index]))]
+                + [oldshape[i] for i in range(index + 1, len(it))]
+            )
+            newblocks[dest_key] = oldblocks[it].reshape(newshape)
+        else:
+            for idmap in [i for i, x in enumerate(mapping[1]) if x == int2split]:
+                valuesplited = mapping[0][idmap]
+                dest_key = tuple(
+                    [it[i] for i in range(index)]
+                    + list(valuesplited)
+                    + [it[i] for i in range(index + 1, len(it))]
+                )
+                oldshape = oldblocks[it].shape
+
+                newsliced = tuple(
+                    [slice(0, oldshape[i]) for i in range(index)]
+                    + [mapping[2][idmap]]
+                    + [slice(0, oldshape[i]) for i in range(index + 1, len(oldshape))]
+                )
+                tmp = oldblocks[it][newsliced]
+
+                deg_map = splitgroup_mapping(model_name, section="deg")
+                dimL = deg_map[mapping[0][idmap][0]]
+                dimR = deg_map[mapping[0][idmap][1]]
+                lastshape = tuple(
+                    [oldshape[i] for i in range(index)]
+                    + [dimL, dimR]
+                    + [oldshape[i] for i in range(index + 1, len(oldshape))]
+                )
+
+                newblocks[dest_key] = tmp.reshape(lastshape)
