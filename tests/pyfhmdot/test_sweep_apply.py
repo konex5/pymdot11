471ec96db4ff3aeeaa3add3ad6a428b72bf97580
konex5 <fix@konex.dev>
2025-02-21 16:23:19 +0100

    add more tests

--- tests/pyfhmdot/test_sweep_apply.py
+++ tests/pyfhmdot/test_sweep_apply.py
@@ -30,3 +30,26 @@ def test_sweep_and_no_apply(make_maximal_entangled_state_u1):
     )
     assert mps[0][(0, 0, 0)][0, 0, 0] == -1
     assert mps[0][(0, 1, 1)][0, 0, 0] == -1
+
+    sweep_and_apply(
+        size=size,
+        start_position=size,
+        end_position=1,
+        mps=mps,
+        gate=[None] * size,
+        apply_UM=apply_UM,
+        apply_MV=apply_MV,
+        apply_gate_UM=apply_UM,
+        apply_gate_MV=apply_MV,
+        start_odd_bonds=True,
+        conserve_left_right=True,
+        simdict={
+            "discarded_weights": 1e-8,
+            "eps_truncation_error": 1e-8,
+            "dw_Dmax": 100,
+            "dw_Dmax_tot": 100,
+            "normalize": True,
+            "dw_one_serie": 0,
+        },
+    )
+    pass
