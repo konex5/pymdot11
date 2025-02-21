65c261e6b58a2d8e5b0dcf346ddee4a5eeee2436
konex5 <fix@konex.dev>
2025-02-21 16:23:19 +0100

    make the dict more c++ compatible

--- pyfhmdot/general.py
+++ pyfhmdot/general.py
@@ -1,23 +1,64 @@
 from numpy import sqrt as _sqrt
 
 
+def translate_qn_name(qn_num):
+    if qn_num == 0:
+        return "no"
+    elif qn_num == 1:
+        return "u1"
+    else:
+        return ""
+
+
+def translate_spin_name(spin_num):
+    if spin_num == 0:
+        return "sh"
+    elif spin_num == 1:
+        return "so"
+    elif spin_num == 2:
+        return "sf"
+    elif spin_num == 3:
+        return "ldsh"
+    else:
+        return ""
+
+
+def get_model_name(hamiltonian_path):
+    return "hamiltonian description"
+
+
 def get_model_info(hamiltonian_path):
-    return {"size": 10, "spin_name": "sh", "qn_name": "u1", "interaction_range": 2}
+    return {"size": 10, "spin_name": 0, "qn_name": 1, "interaction_range": 2}
 
 
-def get_simulation_info():
+def get_model_parameters(hamiltonian_path):
+    return {"J": 1.0, "Jz": 2.0}
+
+
+def get_details_zdmrg():
     return {
-        "dw_weights": 0,
-        "eps_trunkation": 10 ** -8,
+        "eps_truncation": 10 ** -8,
         "chi_max": 600,
-        "conserve_qnum": True,
+        "nb_warmup_sweep": 6,
+        "nb_sweep": 6,
+    }
+
+
+def get_details_tdmrg():
+    return {
+        "discarded_weights": 0,
+        "eps_truncation": 10 ** -8,
+        "chi_max": 600,
+        "delta": 0.025,
+        "store_state": 0.5,
+        "stop_value": 4,
     }
 
 
 def create_maximal_entangled_state(size, spin_name, qn_name, **kwargs):
     from pyfhmdot.pyoperators import single_operator
 
-    operator_name = spin_name + "_id_" + qn_name
+    operator_name = translate_spin_name(spin_name) + "_id_" + translate_qn_name(qn_name)
     coef = []
     dmps = []
     for l in range(size):
