21fc522b2b510ef38acb16fa0bd649fc672fc649
konex5 <fix@konex.dev>
2025-02-21 16:23:23 +0100

    add correlation

--- script/all.sh
+++ script/all.sh
@@ -27,6 +27,8 @@ echo "observables"
 ./script/cli_maverage.py -b /tmp/2B_00.0500.h5 -k /tmp/2B_00.0500.h5 -n sh_sz_u1
 ./script/cli_maverage.py -b /tmp/2B_00.0500.h5 -k /tmp/2B_00.0500.h5 -n sh_sz_u1-sh_sz_u1
 
+./script/cli_maverage.py -b /tmp/2B_00.0250_sz_0005/t_00.0000.h5 -k /tmp/2B_00.0250.h5 -n sh_sz_u1
+
 echo "energies"
 ./script/cli_energy.py -b /tmp/2B_00.0000.h5 -k /tmp/2B_00.0000.h5 -H /tmp/hamiltonian.h5
 ./script/cli_energy.py -b /tmp/2B_00.0250.h5 -k /tmp/2B_00.0250.h5 -H /tmp/hamiltonian.h5
