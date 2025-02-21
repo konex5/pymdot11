8e74784cb463fbb0d0383a6e80fab9ffa13d83a9
konex5 <fix@konex.dev>
2025-02-21 16:23:19 +0100

    add test

--- tests/pyfhmdot/test_operators.py
+++ tests/pyfhmdot/test_operators.py
@@ -1,11 +1,11 @@
 import pytest
 
 
-from pyfhmdot.pyoperators import single_operator_dense
 
 
 def test_single_operator_dense():
-    a = single_operator_dense("sh_id_no", coef=3)[0][(0, 0)]
+    from pyfhmdot.pyoperators import single_operator
+    a = single_operator("sh_id_no", coef=3)[0][(0, 0)]
     assert a[0, 0] == 3 and a[1, 1] == 3
-    a = single_operator_dense("sh_sp_u1", coef=3)[0][(1, 0)]
+    a = single_operator("sh_sp_u1", coef=3)[0][(1, 0)]
     assert a[0, 0] == 3
---
+++ tests/pyfhmdot/test_sweep_apply.py
@@ -0,0 +1,11 @@
+import pytest
+
+from pyfhmdot.algorithm import sweep_and_apply, apply_UM, apply_MV
+
+
+def test_sweep_and_no_apply(make_maximal_entangled_state_u1):
+    size = 7
+    mps = make_maximal_entangled_state_u1(size)
+
+    sweep_and_apply(size=size, start_position=1, end_position=size, mps=mps,
+                    apply_UM=apply_UM, apply_MV=apply_MV, apply_gate_UM=apply_UM, apply_gate_MV=apply_MV,start_odd_bonds=False,conserve_left_right=False)
