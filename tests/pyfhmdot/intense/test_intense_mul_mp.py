925595d7ebcdb5dc796460a61f3f1a9c8333bba9
konex5 <fix@konex.dev>
2025-02-21 16:23:23 +0100

    add dummy test

--- tests/pyfhmdot/intense/test_intense_mul_mp.py
+++ tests/pyfhmdot/intense/test_intense_mul_mp.py
@@ -3,6 +3,10 @@ import pytest
 
 def test_permute_arrays(make_single_dummy_dgate):
     from pyfhmdot.intense.mul_mp import permute_blocs
+    
+    dst_blocs = {}
+    permute_blocs(dst_blocs, {}, [(0, 1, 2, 3), (3, 2, 0, 1)])
+    assert dst_blocs == {}
 
     theta = make_single_dummy_dgate()
     dst_blocs = {}
