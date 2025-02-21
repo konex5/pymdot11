f35f905be92d2f2e67a11d6dad9c7fb553b1fbe8
konex5 <fix@konex.dev>
2025-02-21 16:23:21 +0100

    add contraction

--- pyfhmdot/intense/contract.py
+++ pyfhmdot/intense/contract.py
@@ -1,7 +1,7 @@
-from pyfhmdot.intense.mul_mp import multiply_mp, trace_mp, fuse_mp, permute_blocs
+from pyfhmdot.intense.mul_mp import multiply_mp, fuse_mp
 
 
-def contract_mps_mpo_mps_left(dst,mps_down,mpo,mps_up):
+def contract_mps_mpo_mps_left_border(dst,mps_down,mpo,mps_up):
     mpo_left = {}
     fuse_mp(mpo_left, mpo,0)
     tmp = {}
@@ -25,7 +25,7 @@ def contract_mps_mpo_mps_left(dst,mps_down,mpo,mps_up):
     #    |_|-0
 
  
-def contract_mps_mpo_mps_right(dst,mps_down,mpo,mps_up):
+def contract_mps_mpo_mps_right_border(dst,mps_down,mpo,mps_up):
     mpo_left = {}
     fuse_mp(mpo_left, mpo, 2)
     tmp = {}
@@ -48,3 +48,32 @@ def contract_mps_mpo_mps_right(dst,mps_down,mpo,mps_up):
     # 1 -| |
     #    | |
     # 0 -|_|
+
+ 
+def contract_left_bloc(dst,left_bloc, mps_down,mpo,mps_up):
+    tmp = {}
+    # mps_down
+    #    2|
+    # 0 -|_|-3
+    #    1|
+    multiply_mp(tmp,left_bloc,mps_down,[0],[0])
+    # tmp
+    # 1 -| |
+    # 0 -| ||3
+    #    |___|-4
+    #      2|
+    tmp_tmp = {}
+    multiply_mp(tmp_tmp,tmp,mpo,[0,3],[0,1])
+    # tmp
+    # 0 -| ||3
+    #    |   |-4
+    #    |___|-2
+    #      1|
+    multiply_mp(dst,tmp_tmp,mps_up,[0,1,3],[0,2,1])
+    # dst
+    #    | |- 2
+    #    | |
+    #    | |- 1
+    #    | |
+    #    |_|- 0
+
