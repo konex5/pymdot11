b511cd077c1f611a4cef7278bd4b1de853267836
konex5 <fix@konex.dev>
2025-02-21 16:23:21 +0100

    add test like previous code, new code is working

--- tests/pyfhmdot/test_apply_gate.py
+++ tests/pyfhmdot/test_apply_gate.py
@@ -1,15 +1,20 @@
 import pytest
 
 
-def test_apply_gate_previous_code(single_ab_before,single_tmp_theta,make_single_dummy_dgate,single_ab_after):
-    from pyfhmdot.routine.interface import mm_to_theta_with_gate, theta_to_mm,mm_to_theta_with_gate_to_delete_at_some_point
+def test_apply_gate_previous_code(
+    single_ab_before, single_tmp_theta, make_single_dummy_dgate, single_ab_after
+):
+    from pyfhmdot.routine.interface import (
+        mm_to_theta_with_gate,
+        theta_to_mm,
+        mm_to_theta_with_gate_to_delete_at_some_point,
+    )
     import numpy as np
 
     a_before, b_before = single_ab_before
     tmp_theta = single_tmp_theta
     a_after, b_after = single_ab_after
     gate = make_single_dummy_dgate()
-
 
     tmp_blocs = {}
     mm_to_theta_with_gate_to_delete_at_some_point(
@@ -21,7 +26,7 @@ def test_apply_gate_previous_code(single_ab_before,single_tmp_theta,make_single_
         conserve_left_right_after=False,
     )
     for key in tmp_theta.keys():
-        assert np.all(np.abs(tmp_theta[key] - tmp_blocs[key])<1e-8)
+        assert np.all(np.abs(tmp_theta[key] - tmp_blocs[key]) < 1e-8)
 
     dst_a = {}
     dst_b = {}
@@ -29,15 +34,15 @@ def test_apply_gate_previous_code(single_ab_before,single_tmp_theta,make_single_
         theta_blocs=tmp_blocs,
         lhs_blocs=dst_a,
         rhs_blocs=dst_b,
-        dw_dict={"dw_one_serie":0},
+        dw_dict={"dw_one_serie": 0},
         chi_max=100,
         normalize=True,
-        conserve_direction_left=False,
-        eps=10**-10,
+        conserve_direction_left=True,
+        eps=10 ** -10,
         is_um=True,
     )
 
     for key in a_after.keys():
-        assert np.all(a_after[key] == dst_a[key])
+        assert np.all(np.abs(a_after[key] - dst_a[key]) < 1e-8)
     for key in b_after.keys():
-        assert np.all(b_after[key] == dst_b[key])
+        assert np.all(np.abs(b_after[key] - dst_b[key]) < 1e-8)
