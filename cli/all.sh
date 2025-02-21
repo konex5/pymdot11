4701fd08b4181c0da5a5a0e1e8cca9eec978f5a2
konex5 <fix@konex.dev>
2025-02-21 16:23:21 +0100

    remove trailing whitespace

--- cli/all.sh
+++ cli/all.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-
 ./cli/cli_create_hamiltonian.py -i ./tests/example/model.toml -o /tmp/hamiltonian.h5
 ./cli/cli_create_hamiltonian_gates.py -i tests/example/model.toml -o /tmp/hamiltonian_gates.h5
 ./cli/cli_create_maximal_entangled_state.py -H /tmp/hamiltonian.h5 -o /tmp/2B_00.0000.h5
+
 ./cli/cli_Tdmrg.py -G /tmp/hamiltonian_gates.h5 -M /tmp/2B_00.0000.h5 -o /tmp/
 ./cli/cli_tdmrg.py -G /tmp/hamiltonian_gates.h5 -M /tmp/2B_00.0250.h5 -o /tmp/
+
+./cli/cli_mbracket.py -l /tmp/2B_00.0250.h5 -r /tmp/2B_00.0250_00.0125.h5
+
