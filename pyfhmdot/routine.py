7e91c62252fe1d977d9c6c10d8764f7c4619e566
konex5 <fix@konex.dev>
2025-02-21 16:23:19 +0100

    less is better

--- pyfhmdot/routine.py
+++ pyfhmdot/routine.py
@@ -1,6 +1,3 @@
-import numpy as _np
-from scipy.linalg import svd as _svd
-
 from pyfhmdot.svd_routine import (
     normalize_the_array,
     truncation_strategy,
@@ -141,25 +138,3 @@ def mpsQ_svd_th2mV(thetaQ, mpsL, mpsR, simdict, **kwargs):
         mpsL,
         mpsR,
     )
-    pass
-
-
-# def merge_indices_according_to_qnum(qname, theta_blocs_no_merge):
-#     # previously called subnewsize
-#     pass
-#
-#
-# def fast_UM_no_mult():
-#     pass
-#
-#
-# def fast_MV_no_mult():
-#     pass
-#
-#
-# def theta_to_um(theta_blocs, deg_indices_and_slices, eps_trunk):
-#     pass
-#
-#
-# def theta_to_mv(theta_blocs, deg_indices_and_slices, eps_trunk):
-#     pass
