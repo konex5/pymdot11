18d666690afe2e39934847e9a5d7a2d51d3784e8
konex5 <fix@konex.dev>
2025-02-21 16:23:20 +0100

    remove duplicate

--- pyfhmdot/mul_routine.py
+++ pyfhmdot/mul_routine.py
@@ -61,21 +61,34 @@ def mul_mv_nondeg(
     nondeg_dims: _List,
     dst_lhs_blocs: _Dict[_Tuple[int, int, int], _np.ndarray],
     dst_rhs_blocs: _Dict[_Tuple[int, int, int], _np.ndarray],
+    is_um: bool = False,
 ) -> None:
-    i_Nb = len(nondeg)
-    for i in range(i_Nb):  # reversed, and passed by pop.
+    for i in range(len(nondeg)):  # reversed, and passed by pop.
         Dsi = cut.pop()
         if Dsi > 0:
             dims = nondeg_dims.pop()
             tmp_nondeg = nondeg.pop()
+            if is_um:
+                # U
+                mat_left = array_U[i][:, :Dsi].reshape(dims[0], dims[1], Dsi)
+                # M
+                mat_right = _np.dot(
+                    _np.diag(array_S[i][:Dsi]), array_V[i][:Dsi, :]
+                ).reshape(Dsi, dims[2], dims[3])
+            else:
+                # M
+                mat_left = _np.dot(
+                    array_U.pop()[:, :Dsi], _np.diag(array_S.pop()[:Dsi])
+                ).reshape(dims[0], dims[1], Dsi)
+                # V
+                mat_right = array_V.pop()[:Dsi, :].reshape(Dsi, dims[2], dims[3])
+
             dst_lhs_blocs[
                 (tmp_nondeg[1][0], tmp_nondeg[1][1], tmp_nondeg[0])
-            ] = _np.dot(array_U.pop()[:, :Dsi], _np.diag(array_S.pop()[:Dsi])).reshape(
-                dims[0], dims[1], Dsi
-            )
+            ] = mat_left
             dst_rhs_blocs[
                 (tmp_nondeg[0], tmp_nondeg[1][2], tmp_nondeg[1][3])
-            ] = array_V.pop()[:Dsi, :].reshape(Dsi, dims[2], dims[3])
+            ] = mat_right
         else:
             array_U.pop()
             array_V.pop()
@@ -85,24 +98,27 @@ def mul_mv_nondeg(
 
 
 def mul_um_nondeg(
-    array_U, array_S, cut, array_V, nondeg, nondeg_dims, dst_lhs_blocs, dst_rhs_blocs
+    array_U,
+    array_S,
+    cut,
+    array_V,
+    nondeg,
+    nondeg_dims,
+    dst_lhs_blocs,
+    dst_rhs_blocs,
+    is_um=True,
 ):
-    for i in range(len(nondeg)):  # reversed, and passed by pop.
-        Dsi = cut[i]
-        if Dsi != 0:
-            dims = nondeg_dims[i]
-            dst_lhs_blocs[(nondeg[i][1][0], nondeg[i][1][1], nondeg[i][0])] = array_U[
-                i
-            ][:, :Dsi].reshape(dims[0], dims[1], Dsi)
-            dst_rhs_blocs[(nondeg[i][0], nondeg[i][1][2], nondeg[i][1][3])] = _np.dot(
-                _np.diag(array_S[i][:Dsi]), array_V[i][:Dsi, :]
-            ).reshape(Dsi, dims[2], dims[3])
-        else:
-            array_U.pop()
-            array_V.pop()
-            array_S.pop()
-            nondeg_dims.pop()
-            nondeg.pop()
+    mul_mv_nondeg(
+        array_U,
+        array_S,
+        cut,
+        array_V,
+        nondeg,
+        nondeg_dims,
+        dst_lhs_blocs,
+        dst_rhs_blocs,
+        is_um=is_um,
+    )
 
 
 def mul_mv_deg(
@@ -114,7 +130,7 @@ def mul_mv_deg(
     subnewsize: _List,
     dst_lhs_blocs: _Dict[_Tuple[int, int, int], _np.ndarray],
     dst_rhs_blocs: _Dict[_Tuple[int, int, int], _np.ndarray],
-    is_um: bool=False
+    is_um: bool = False,
 ) -> None:
     i_Nb = len(deg)  # index for deg and subnewsize.. we
     for i in range(i_Nb):  # reversed, and pop each value.
@@ -124,10 +140,14 @@ def mul_mv_deg(
                 # U
                 mat_left = array_U.pop()  # [:,:Dsi]
                 # M
-                mat_right = _np.dot(_np.diag(array_S.pop()[:Dsi]), array_V.pop()[:Dsi, :])
+                mat_right = _np.dot(
+                    _np.diag(array_S.pop()[:Dsi]), array_V.pop()[:Dsi, :]
+                )
             else:
                 # M
-                mat_left = _np.dot(array_U.pop()[:, :Dsi], _np.diag(array_S.pop()[:Dsi]))
+                mat_left = _np.dot(
+                    array_U.pop()[:, :Dsi], _np.diag(array_S.pop()[:Dsi])
+                )
                 # V
                 mat_right = array_V.pop()  # [Dsi:,:]
 
@@ -155,37 +175,25 @@ def mul_mv_deg(
             deg.pop()
 
 
-
-
 def mul_um_deg(
-    array_U, array_S, cut, array_V, deg, subnewsize, dst_lhs_blocs, dst_rhs_blocs
+    array_U,
+    array_S,
+    cut,
+    array_V,
+    deg,
+    subnewsize,
+    dst_lhs_blocs,
+    dst_rhs_blocs,
+    is_um=True,
 ):
-    i_Nb = len(deg)
-    for i in range(i_Nb):
-        Dsi = cut.pop()
-        if Dsi > 0:
-            M = _np.dot(_np.diag(array_S.pop()[:Dsi]), array_V.pop()[:Dsi, :])
-            U = array_U.pop()  # [:,:Dsi]
-
-            tmp = subnewsize.pop()
-            tmp_deg = deg.pop()
-            for it in tmp_deg[1]:
-                posL = tmp[2].index((it[0], it[1]))
-                offL = tmp[3][posL]
-                dimL = tmp[4][posL]
-                posR = tmp[5].index((it[2], it[3]))
-                offR = tmp[6][posR]
-                dimR = tmp[7][posR]
-
-                dst_lhs_blocs[(it[0], it[1], tmp_deg[0])] = U[
-                    slice(offL, offL + dimL[0] * dimL[1]), :Dsi
-                ].reshape(dimL[0], dimL[1], Dsi)
-                dst_rhs_blocs[(tmp_deg[0], it[2], it[3])] = M[
-                    :Dsi, slice(offR, offR + dimR[0] * dimR[1])
-                ].reshape(Dsi, dimR[0], dimR[1])
-        else:
-            array_U.pop()
-            array_V.pop()
-            array_S.pop()
-            subnewsize.pop()
-            deg.pop()
+    mul_mv_deg(
+        array_U,
+        array_S,
+        cut,
+        array_V,
+        deg,
+        subnewsize,
+        dst_lhs_blocs,
+        dst_rhs_blocs,
+        is_um=is_um,
+    )
