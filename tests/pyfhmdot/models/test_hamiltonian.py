23167f3827c55aa8df55118a5db09505a2315114
konex5 <fix@konex.dev>
2025-02-21 16:23:22 +0100

    add test_hamiltonian

--- tests/pyfhmdot/models/test_hamiltonian.py
+++ tests/pyfhmdot/models/test_hamiltonian.py
@@ -47,3 +47,15 @@ def test_hamiltonian_obc():
     multiply_mp(dst, tmp, ham[2], [5], [0])
     assert dst[(0, 0, 0, 0, 1, 0, 1, 0)][0, 0, 0, 0, 0, 0, 0, 0] == 400
     dst.clear()
+    
+    # submodel
+    ham = hamiltonian_obc("sh_xxz-hz_u1", {"Jxy":0,"Jz":0, "hz": 7.0}, 2)
+    assert ham[0][(0, 1, 1, 0)][0, 0, 0, 0] == 7
+    assert ham[1][(0, 1, 1, 0)][0, 0, 0, 0] == 1
+    assert ham[0][(0, 1, 1, 4)][0, 0, 0, 0] == 1
+    assert ham[1][(4, 1, 1, 0)][0, 0, 0, 0] == 7
+
+    dst = {}
+    multiply_mp(dst, ham[0], ham[1], [3], [0])
+    assert dst[(0, 0, 0, 0, 0, 0)][0, 0, 0, 0, 0, 0] == -14
+    dst.clear()
