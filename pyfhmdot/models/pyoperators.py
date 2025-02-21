ee9c948a6523c4b8fcf857033b27d23d88608b4b
konex5 <fix@konex.dev>
2025-02-21 16:23:20 +0100

    add rung and remove legacy code

--- pyfhmdot/models/pyoperators.py
+++ pyfhmdot/models/pyoperators.py
@@ -161,12 +161,69 @@ def single_operator(name, coef):  # -> one bloc
             (0, 1): coef * _np.array([[1, 0]], dtype=_real_t),
             (1, 2): coef * _np.array([[0], [1]], dtype=_real_t),
         },
+        "ru_smid_no": {
+            (0, 0): coef
+            * _np.array(
+                [[0, 0, 0, 0], [0, 0, 0, 0], [1, 0, 0, 0], [0, 1, 0, 0]], dtype=_real_t
+            )
+        },
+        "ru_smid_u1": {
+            (1, 0): coef * _np.array([[0], [1]], dtype=_real_t),
+            (2, 1): coef * _np.array([[1, 0]], dtype=_real_t),
+        },
+        "ru_idsm_no": {
+            (0, 0): coef
+            * _np.array(
+                [[0, 0, 0, 0], [1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 1, 0]], dtype=_real_t
+            )
+        },
+        "ru_idsm_u1": {
+            (1, 0): coef * _np.array([[1], [0]], dtype=_real_t),
+            (2, 1): coef * _np.array([[0, 1]], dtype=_real_t),
+        },
+        "ru_spsp_no": {
+            (0, 0): coef
+            * _np.array(
+                [[0, 0, 0, 1], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]], dtype=_real_t
+            )
+        },
+        "ru_spsp_u1": {
+            (0, 2): coef * _np.array([[1]], dtype=_real_t),
+        },
+        "ru_smsm_no": {
+            (0, 0): coef
+            * _np.array(
+                [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [1, 0, 0, 0]], dtype=_real_t
+            )
+        },
+        "ru_smsm_u1": {
+            (2, 0): coef * _np.array([[1]], dtype=_real_t),
+        },
+        "ru_spsm_no": {
+            (0, 0): coef
+            * _np.array(
+                [[0, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 0], [0, 0, 0, 0]], dtype=_real_t
+            )
+        },
+        "ru_spsm_u1": {
+            (1, 1): coef * _np.array([[0, 1], [0, 0]], dtype=_real_t),
+        },
+        "ru_smsp_no": {
+            (0, 0): coef
+            * _np.array(
+                [[0, 0, 0, 0], [0, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 0]], dtype=_real_t
+            )
+        },
+        "ru_smsp_u1": {
+            (1, 1): coef * _np.array([[0, 0], [1, 0]], dtype=_real_t),
+        },
     }
 
     return operators[name]
 
 
 def two_sites_bond_operator(name, coef, *, weight_on_left=None):  # -> two blocs
+    
     bonds = [
         "sh_id_no-sh_id_no",
         "sh_sm_no-sh_sp_no",
@@ -188,7 +245,50 @@ def two_sites_bond_operator(name, coef, *, weight_on_left=None):  # -> two blocs
         "so_sm_u1-so_sp_u1",
         "so_sp_u1-so_sm_u1",
         "so_sz_u1-so_sz_u1",
+        "ru_idid_no-ru_idid_no",
+        "ru_idsz_no-ru_idsz_no",
+        "ru_szid_no-ru_idsz_no",
+        "ru_idsz_no-ru_szid_no",
+        "ru_szid_no-ru_szid_no",
+        "ru_idsp_no-ru_idsp_no",
+        "ru_spid_no-ru_idsp_no",
+        "ru_idsp_no-ru_spid_no",
+        "ru_spid_no-ru_spid_no",
+        "ru_idsm_no-ru_idsm_no",
+        "ru_smid_no-ru_idsm_no",
+        "ru_idsm_no-ru_smid_no",
+        "ru_smid_no-ru_smid_no",
+        "ru_idsp_no-ru_idsm_no",
+        "ru_spid_no-ru_idsm_no",
+        "ru_idsp_no-ru_smid_no",
+        "ru_spid_no-ru_smid_no",
+        "ru_idsm_no-ru_idsp_no",
+        "ru_smid_no-ru_idsp_no",
+        "ru_idsm_no-ru_spid_no",
+        "ru_smid_no-ru_spid_no",
+        "ru_idid_u1-ru_idid_u1",
+        "ru_idsz_u1-ru_idsz_u1",
+        "ru_szid_u1-ru_idsz_u1",
+        "ru_idsz_u1-ru_szid_u1",
+        "ru_szid_u1-ru_szid_u1",
+        "ru_idsp_u1-ru_idsp_u1",
+        "ru_spid_u1-ru_idsp_u1",
+        "ru_idsp_u1-ru_spid_u1",
+        "ru_spid_u1-ru_spid_u1",
+        "ru_idsm_u1-ru_idsm_u1",
+        "ru_smid_u1-ru_idsm_u1",
+        "ru_idsm_u1-ru_smid_u1",
+        "ru_smid_u1-ru_smid_u1",
+        "ru_idsp_u1-ru_idsm_u1",
+        "ru_spid_u1-ru_idsm_u1",
+        "ru_idsp_u1-ru_smid_u1",
+        "ru_spid_u1-ru_smid_u1",
+        "ru_idsm_u1-ru_idsp_u1",
+        "ru_smid_u1-ru_idsp_u1",
+        "ru_idsm_u1-ru_spid_u1",
+        "ru_smid_u1-ru_spid_u1",
     ]
+
     if name in bonds:
         left_name, right_name = name.split("-")
 
@@ -200,433 +300,3 @@ def two_sites_bond_operator(name, coef, *, weight_on_left=None):  # -> two blocs
             return single_operator(left_name, coef), single_operator(right_name, 1.0)
         else:
             return single_operator(left_name, 1.0), single_operator(right_name, coef)
-
-
-"""
-    # ### ldsh=ladder spin half
-    # # the numbers correspond to legs
-    # ## ldsh-U1detached <<<<<<<<<<< PROBLEM! Sz_tot cons impossible
-    # # 0 is |s1=1/2,mz1=-1/2>x|s2=1/2,mz2=-1/2>
-    # # 1 is |s1=1/2,mz1=-1/2>x|s2=1/2,mz2=+1/2>
-    # # 2 is |s1=1/2,mz1=+1/2>x|s2=1/2,mz2=-1/2>
-    # # 3 is |s1=1/2,mz1=+1/2>x|s2=1/2,mz2=+1/2>
-    # ## ldsh-U1comb => WORK!
-
-    ############################################
-    # ### PLEASE CHECK VERY CAREFULLY SINCE
-    "ldsh-Id": {  # ok
-        "nb_site": 1,
-        "coef": 2.0,
-        "qBasis": {
-            "ldsh-None": [
-                [
-                    (0, 0),
-                    (1 / 2.0)
-                    * _np.array(
-                        [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]],
-                        dtype=_real_t,
-                    ),
-                ]
-            ],
-            "ldsh-U1comb": [
-                [(0, 0), _np.array([[+1 / 2.0]], dtype=_real_t)],
-                [(1, 1), _np.array([[+1 / 2.0, 0], [0, +1 / 2.0]], dtype=_real_t)],
-                [(2, 2), _np.array([[+1 / 2.0]], dtype=_real_t)],
-            ],
-        },
-        "qchange": {
-            "ldsh-None": [(0,)],
-            "ldsh-U1detached": [(0,)],
-            "ldsh-U1comb": [(0,)],
-        },
-    },
-    "ldsh-SzId": {  # ok
-        "nb_site": 1,
-        "coef": 1.0,
-        "qBasis": {
-            "ldsh-None": [
-                [
-                    (0, 0),
-                    (1 / 2.0)
-                    * _np.array(
-                        [[-1, 0, 0, 0], [0, -1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]],
-                        dtype=_real_t,
-                    ),
-                ]
-            ],
-            "ldsh-U1comb": [
-                [(0, 0), _np.array([[-1 / 2.0]], dtype=_real_t)],
-                [(1, 1), _np.array([[-1 / 2.0, 0], [0, +1 / 2.0]], dtype=_real_t)],
-                [(2, 2), _np.array([[+1 / 2.0]], dtype=_real_t)],
-            ],
-        },
-        "qchange": {
-            "ldsh-None": [(0,)],
-            "ldsh-U1detached": [(0,)],
-            "ldsh-U1comb": [(0,)],
-        },
-    },
-    "ldsh-IdSz": {  # ok
-        "nb_site": 1,
-        "coef": 1.0,
-        "qBasis": {
-            "ldsh-None": [
-                [
-                    (0, 0),
-                    (1 / 2.0)
-                    * _np.array(
-                        [[-1, 0, 0, 0], [0, +1, 0, 0],
-                            [0, 0, -1, 0], [0, 0, 0, +1]],
-                        dtype=_real_t,
-                    ),
-                ]
-            ],
-            "ldsh-U1comb": [
-                [(0, 0), _np.array([[-1 / 2.0]], dtype=_real_t)],
-                [(1, 1), _np.array([[+1 / 2.0, 0], [0, -1 / 2.0]], dtype=_real_t)],
-                [(2, 2), _np.array([[+1 / 2.0]], dtype=_real_t)],
-            ],
-        },
-        "qchange": {
-            "ldsh-None": [(0,)],
-            "ldsh-U1detached": [(0,)],
-            "ldsh-U1comb": [(0,)],
-        },
-    },
-    "ldsh-SzSz": {  # ok
-        "nb_site": 1,
-        "coef": 1 / 2.0,
-        "qBasis": {
-            "ldsh-None": [
-                [
-                    (0, 0),
-                    (1 / 2.0)
-                    * _np.array(
-                        [[+1, 0, 0, 0], [0, -1, 0, 0],
-                            [0, 0, -1, 0], [0, 0, 0, +1]],
-                        dtype=_real_t,
-                    ),
-                ]
-            ],
-            "ldsh-U1comb": [
-                [(0, 0), _np.array([[+1 / 2.0]], dtype=_real_t)],
-                [(1, 1), _np.array([[-1 / 2.0, 0], [0, -1 / 2.0]], dtype=_real_t)],
-                [(2, 2), _np.array([[+1 / 2.0]], dtype=_real_t)],
-            ],
-        },
-        "qchange": {
-            "ldsh-None": [(0,)],
-            "ldsh-U1detached": [(0,)],
-            "ldsh-U1comb": [(0,)],
-        },
-    },
-    "ldsh-SpId": {  # ok
-        "nb_site": 1,
-        "coef": _np.sqrt(2),
-        "qBasis": {
-            "ldsh-None": [
-                [
-                    (0, 0),
-                    (1 / _np.sqrt(2))
-                    * _np.array(
-                        [[0, 0, 0, 0], [0, 0, 0, 0], [+1, 0, 0, 0], [0, +1, 0, 0]],
-                        dtype=_real_t,
-                    ),
-                ]
-            ],
-
-            "ldsh-U1comb": [
-                [(1, 0), _np.array([[0], [+1 / _np.sqrt(2)]], dtype=_real_t)],
-                [(2, 1), _np.array([[+1 / _np.sqrt(2), 0]], dtype=_real_t)],
-            ],
-        },
-        "qchange": {
-            "ldsh-None": [(0,)],
-            "ldsh-U1detached": [(2,)],
-            "ldsh-U1comb": [(2,)],
-        },
-    },
-    "ldsh-IdSp": {  # ok
-        "nb_site": 1,
-        "coef": _np.sqrt(2),
-        "qBasis": {
-            "ldsh-None": [
-                [
-                    (0, 0),
-                    (1 / _np.sqrt(2))
-                    * _np.array(
-                        [[0, 0, 0, 0], [1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 1, 0]],
-                        dtype=_real_t,
-                    ),
-                ]
-            ],
-
-            "ldsh-U1comb": [
-                [(1, 0), _np.array([[+1 / _np.sqrt(2)], [0]], dtype=_real_t)],
-                [(2, 1), _np.array([[0, +1 / _np.sqrt(2)]], dtype=_real_t)],
-            ],
-        },
-        "qchange": {
-            "ldsh-None": [(0,)],
-            "ldsh-U1detached": [(2,)],
-            "ldsh-U1comb": [(2,)],
-        },
-    },
-    "ldsh-SmId": {  # ok
-        "nb_site": 1,
-        "coef": _np.sqrt(2),
-        "qBasis": {
-            "ldsh-None": [
-                [
-                    (0, 0),
-                    (1 / _np.sqrt(2))
-                    * _np.array(
-                        [[0, 0, 1, 0], [0, 0, 0, 1], [0, 0, 0, 0], [0, 0, 0, 0]],
-                        dtype=_real_t,
-                    ),
-                ]
-            ],
-  
-            "ldsh-U1comb": [
-                [(0, 1), _np.array([[0, +1 / _np.sqrt(2)]], dtype=_real_t)],
-                [(1, 2), _np.array([[+1 / _np.sqrt(2)], [0]], dtype=_real_t)],
-            ],
-        },
-        "qchange": {
-            "ldsh-None": [(0,)],
-            "ldsh-U1detached": [(-2,)],
-            "ldsh-U1comb": [(-2,)],
-        },
-    },
-    "ldsh-IdSm": {  # ok
-        "nb_site": 1,
-        "coef": _np.sqrt(2),
-        "qBasis": {
-            "ldsh-None": [
-                [
-                    (0, 0),
-                    (1 / _np.sqrt(2))
-                    * _np.array(
-                        [[0, 1, 0, 0], [0, 0, 0, 0], [0, 0, 0, 1], [0, 0, 0, 0]],
-                        dtype=_real_t,
-                    ),
-                ]
-            ],
-
-            "ldsh-U1comb": [
-                [(0, 1), _np.array([[+1 / _np.sqrt(2), 0]], dtype=_real_t)],
-                [(1, 2), _np.array([[0], [+1 / _np.sqrt(2)]], dtype=_real_t)],
-            ],
-        },
-        "qchange": {
-            "ldsh-None": [(0,)],
-            "ldsh-U1detached": [(-2,)],
-            "ldsh-U1comb": [(-2,)],
-        },
-    },
-    "ldsh-SpSp": {  # ok
-        "nb_site": 1,
-        "coef": 1,
-        "qBasis": {
-            "ldsh-None": [
-                [
-                    (0, 0),
-                    _np.array(
-                        [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [1, 0, 0, 0]],
-                        dtype=_real_t,
-                    ),
-                ]
-            ],
-            "ldsh-U1comb": [[(2, 0), _np.array([[+1]], dtype=_real_t)]],
-        },
-        "qchange": {
-            "ldsh-None": [(0,)],
-            "ldsh-U1detached": [(+4,)],
-            "ldsh-U1comb": [(+4,)],
-        },
-    },
-    "ldsh-SmSm": {  # ok
-        "nb_site": 1,
-        "coef": 1,
-        "qBasis": {
-            "ldsh-None": [
-                [
-                    (0, 0),
-                    _np.array(
-                        [[0, 0, 0, 1], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]],
-                        dtype=_real_t,
-                    ),
-                ]
-            ],
-            "ldsh-U1comb": [[(0, 2), _np.array([[+1]], dtype=_real_t)]],
-        },
-        "qchange": {
-            "ldsh-None": [(0,)],
-            "ldsh-U1detached": [(-4,)],
-            "ldsh-U1comb": [(-4,)],
-        },
-    },
-    "ldsh-SpSm": {  # ok
-        "nb_site": 1,
-        "qchange": [[0, 0]],
-        "coef": 1,
-        "qBasis": {
-            "ldsh-None": [
-                [
-                    (0, 0),
-                    _np.array(
-                        [[0, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 0], [0, 0, 0, 0]],
-                        dtype=_real_t,
-                    ),
-                ]
-            ],
-            "ldsh-U1comb": [[(1, 1), _np.array([[0, +1], [0, 0]], dtype=_real_t)]],
-        },
-        "qchange": {
-            "ldsh-None": [(0,)],
-            "ldsh-U1detached": [(0,)],
-            "ldsh-U1comb": [(0,)],
-        },
-    },
-    "ldsh-SmSp": {  # ok
-        "nb_site": 1,
-        "coef": 1,
-        "qBasis": {
-            "ldsh-None": [
-                [
-                    (0, 0),
-                    _np.array(
-                        [[0, 0, 0, 0], [0, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 0]],
-                        dtype=_real_t,
-                    ),
-                ]
-            ],
-            "ldsh-U1comb": [[(1, 1), _np.array([[0, 0], [+1, 0]], dtype=_real_t)]],
-        },
-        "qchange": {
-            "ldsh-None": [(0,)],
-            "ldsh-U1detached": [(0,)],
-            "ldsh-U1comb": [(0,)],
-        },
-    },
-    "ldsh-SzIdoSzId": {  # ok
-        "nb_site": 2,
-        0: "ldsh-SzId",
-        1: "ldsh-SzId",
-        "qchange": {
-            "ldsh-None": [(0,), (0,)],
-            "ldsh-U1comb": [(0,), (0,)],
-        },
-    },
-    "ldsh-IdSzoIdSz": {  # ok
-        "nb_site": 2,
-        0: "ldsh-IdSz",
-        1: "ldsh-IdSz",
-        "qchange": {
-            "ldsh-None": [(0,), (0,)],
-            "ldsh-U1comb": [(0,), (0,)],
-        },
-    },
-    "ldsh-SzIdoIdSz": {  # ok
-        "nb_site": 2,
-        0: "ldsh-SzId",
-        1: "ldsh-IdSz",
-        "qchange": {
-            "ldsh-None": [(0,), (0,)],
-            "ldsh-U1detached": [(0,), (0,)],
-            "ldsh-U1comb": [(0,), (0,)],
-        },
-    },
-    "ldsh-IdSzoSzId": {  # ok
-        "nb_site": 2,
-        0: "ldsh-IdSz",
-        1: "ldsh-SzId",
-        "qchange": {
-            "ldsh-None": [(0,), (0,)],
-            "ldsh-U1detached": [(0,), (0,)],
-            "ldsh-U1comb": [(0,), (0,)],
-        },
-    },
-    "ldsh-SpIdoSmId": {
-        "nb_site": 2,
-        0: "ldsh-SpId",
-        1: "ldsh-SmId",
-        "qchange": {
-            "ldsh-None": [(0,), (0,)],
-            "ldsh-U1detached": [(+2,), (-2,)],
-            "ldsh-U1comb": [(+2,), (-2,)],
-        },
-    },
-    "ldsh-IdSpoIdSm": {
-        "nb_site": 2,
-        0: "ldsh-IdSp",
-        1: "ldsh-IdSm",
-        "qchange": {
-            "ldsh-None": [(0,), (0,)],
-            "ldsh-U1detached": [(+2,), (-2,)],
-            "ldsh-U1comb": [(+2,), (-2,)],
-        },
-    },
-    "ldsh-SpIdoIdSm": {
-        "nb_site": 2,
-        0: "ldsh-SpId",
-        1: "ldsh-IdSm",
-        "qchange": {
-            "ldsh-None": [(0,), (0,)],
-            "ldsh-U1detached": [(+2,), (-2,)],
-            "ldsh-U1comb": [(+2,), (-2,)],
-        },
-    },
-    "ldsh-IdSpoSmId": {
-        "nb_site": 2,
-        0: "ldsh-IdSp",
-        1: "ldsh-SmId",
-        "qchange": {
-            "ldsh-None": [(0,), (0,)],
-            "ldsh-U1detached": [(+2,), (-2,)],
-            "ldsh-U1comb": [(+2,), (-2,)],
-        },
-    },
-    "ldsh-SmIdoSpId": {
-        "nb_site": 2,
-        0: "ldsh-SmId",
-        1: "ldsh-SpId",
-        "qchange": {
-            "ldsh-None": [(0,), (0,)],
-            "ldsh-U1detached": [(-2,), (+2,)],
-            "ldsh-U1comb": [(-2,), (+2,)],
-        },
-    },
-    "ldsh-IdSmoIdSp": {
-        "nb_site": 2,
-        0: "ldsh-IdSm",
-        1: "ldsh-IdSp",
-        "qchange": {
-            "ldsh-None": [(0,), (0,)],
-            "ldsh-U1detached": [(-2,), (+2,)],
-            "ldsh-U1comb": [(-2,), (+2,)],
-        },
-    },
-    "ldsh-SmIdoIdSp": {
-        "nb_site": 2,
-        0: "ldsh-SmId",
-        1: "ldsh-IdSp",
-        "qchange": {
-            "ldsh-None": [(0,), (0,)],
-            "ldsh-U1detached": [(-2,), (+2,)],
-            "ldsh-U1comb": [(-2,), (+2,)],
-        },
-    },
-    "ldsh-IdSmoSpId": {
-        "nb_site": 2,
-        0: "ldsh-IdSm",
-        1: "ldsh-SpId",
-        "qchange": {
-            "ldsh-None": [(0,), (0,)],
-            "ldsh-U1detached": [(-2,), (+2,)],
-            "ldsh-U1comb": [(-2,), (+2,)],
-        },
-    },
-
-
-"""
